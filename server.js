3d62797a54a993df16b767e0b5f58df2a468dd6d
alexrehopkins <51701886+alexrehopkins@users.noreply.github.com>
2019-10-19 21:45:38 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const Datastore = require('nedb');
-
+const port = process.env.PORT || 3000;
 const app = express();
-app.listen(3000, () => console.log('listen at 3000'));
+app.listen(port, () => console.log('listening'));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
